70e3ee4510b07f3224af680b3e716873792f0be2
Abdulhafeez Hameed <laxhameedhafeez@gmail.com>
2021-11-02 19:14:38 +0100

    fixed type Array

--- models/country.js
+++ models/country.js
@@ -3,16 +3,16 @@ import mongoose from 'mongoose';
 const countrySchema = new mongoose.Schema({
   name: { type: String },
   alpha: { type: String },
-  topLevelDomain: [{ type: String }],
+  topLevelDomain: { type: Array },
   capital: { type: String },
   subregion: { type: String },
   region: { type: String },
   population: { type: Number },
-  borders: [{ type: String }],
+  borders: { type: Array },
   nativeName: { type: String },
   flag: { type: String },
-  currencies: [{ type: String }],
-  languages: [{ type: String }],
+  currencies: { type: Array },
+  languages: { type: Array },
 });
 
 const Country = mongoose.model('Country', countrySchema);
